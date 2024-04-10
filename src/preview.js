2f660b5798f7aff651d5fee9fff1bd984402df6b
Simone Franchina <simone@italianonprofit.it>
2024-04-10 09:59:04 +0200

    fix: generator

--- src/preview.js
+++ src/preview.js
@@ -1,94 +1,87 @@
 const fs = require('fs');
 const path = require('path');
+const slugify = require('slugify');
+const {JSDOM} = require('jsdom');
+const xmlSerializer = new (require('xmldom').XMLSerializer)();
+const cliProgress = require('cli-progress');
+
+// crea una nuova barra di avanzamento
+const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+
+// get the path to the icons folder
+const iconsPath = path.join(__dirname, './../icons/SVG');
 const distPath = path.join(__dirname, './../dist');
 const cssFilePath = path.join(distPath, 'icons.css');
-const icons = JSON.parse(fs.readFileSync(path.join(__dirname, './list.json'), 'utf-8'));
-const classPrefix = 'sa';
-let cssContent = '';
-
-
-const iconSizes = {
-    'xs': '12px',
-    'sm': '16px',
-    'md': '24px',
-    'lg': '32px',
-    'xl': '48px',
-    '2xl': '64px',
-    '3xl': '96px',
-    '4xl': '128px',
-    '5xl': '192px',
-    '6xl': '256px',
-    '7xl': '384px',
-    '8xl': '512px'
-}
 
 
 
-cssContent += `:root {\n`;
-cssContent += `--icon-color: #000000;\n`;
-cssContent += `}\n`;
 
-cssContent += `.${classPrefix} {\n`;
-cssContent += `  display: inline-block;\n`;
-cssContent += `  mask-size: contain;\n`;
-cssContent += `  -webkit-mask-size: contain;\n`;
-cssContent += `  mask-repeat: no-repeat;\n`;
-cssContent += `  -webkit-mask-repeat: no-repeat;\n`;
-cssContent += `  mask-position: center;\n`;
-cssContent += `  -webkit-mask-position: center;\n`;
-cssContent += ` background-color : var(--icon-color);\n`;
-cssContent += ` width: 24px;\n`;
-cssContent += ` height: 24px;\n`;
-cssContent += `}\n`;
 
-Object.entries(iconSizes).forEach(([size, value]) => {
-    cssContent += `.${classPrefix}-${size} {\n`;
-    cssContent += `  width: ${value};\n`;
-    cssContent += `  height: ${value};\n`;
-    cssContent += `}\n`;
-})
+// inizia la barra di avanzamento
+progressBar.start(100, 0);
 
-for (const category in icons) {
-    for (const icon of icons[category]) {
-        const className = `.${classPrefix}.${classPrefix}-${icon.slug}.${icon.category}`;
-        cssContent += `${className} {\n`;
-        cssContent += `  mask-image: url('${icon.fullPath}');\n`;
-        cssContent += `  -webkit-mask-image: url('${icon.fullPath}');\n`;
-        cssContent += `}\n`;
+
+const getIconsFromDir = (dir, category) => {
+    let icons = [];
+    const files = fs.readdirSync(dir);
+    for (let i = 0; i < files.length; i++) {
+        const fullPath = path.join(dir, files[i]);
+        if (fs.lstatSync(fullPath).isDirectory()) {
+            icons = icons.concat(getIconsFromDir(fullPath, category));
+        } else if (files[i].endsWith('.svg')) {
+            const svgString = fs.readFileSync(fullPath, 'utf-8');
+            const dom = new JSDOM(svgString, {contentType: 'image/svg+xml'});
+            const svgDoc = dom.window.document;
+            const paths = svgDoc.querySelectorAll('path');
+            paths.forEach(path => {
+                let fill;
+                if (path.hasAttribute('fill')) {
+                    if (path.hasAttribute('opacity')) {
+                        const opacity = parseFloat(path.getAttribute('opacity'));
+                        const grayValue = Math.floor(opacity * 255).toString(16).padStart(2, '0');
+                        fill = `#${grayValue}${grayValue}${grayValue}`;
+                    } else {
+                        fill = '#000000';
+                    }
+                    path.setAttribute('fill', fill);
+                }
+            });
+            const modifiedSvgString = xmlSerializer.serializeToString(svgDoc);
+            const distSvgPath = path.join(distPath, "icons" ,path.relative(iconsPath, fullPath));
+            fs.mkdirSync(path.dirname(distSvgPath), {recursive: true});
+            fs.writeFileSync(distSvgPath, modifiedSvgString);
+
+            const fileName = files[i].replace('.svg', '');
+            const relativePath = path.relative(path.dirname(cssFilePath), distSvgPath);
+            icons.push({
+                fullPath: relativePath,
+                fileName,
+                slug: slugify(fileName, {lower: true, strict : true, remove: /[*+~.()'"!:@]/g}),
+                category: category.toLowerCase()
+            });
+            // aggiorna la barra di avanzamento
+            progressBar.update((i / files.length) * 100);
+        }
+
     }
+
+    return icons;
 }
 
-fs.writeFileSync(cssFilePath, cssContent);
-
-
-// Generate HTML preview
-let htmlContent = `
-<!DOCTYPE html>
-<html>
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="icons.css">
-</head>
-<body>
-`;
-
-for (const category in icons) {
-    htmlContent += `<h2>${category}</h2>\n`;
-    htmlContent += `<div class="row">\n`;
-    for (const icon of icons[category]) {
-        const className = `${classPrefix} ${classPrefix}-${icon.slug} ${icon.category}`;
-        htmlContent += `<div class="col">\n`;
-        htmlContent += `<div class="${className}"></div>\n`;
-        htmlContent += `<p>${className}</p>\n`;
-        htmlContent += `</div>\n`;
+const iconsIterator = (dir) => {
+    let icons = {};
+    const files = fs.readdirSync(dir);
+    for (let i = 0; i < files.length; i++) {
+        const fullPath = path.join(dir, files[i]);
+        if (fs.lstatSync(fullPath).isDirectory()) {
+            icons[files[i]] = getIconsFromDir(fullPath, files[i]);
+        }
     }
-    htmlContent += `</div>\n`;
+    progressBar.stop();
+    return icons;
 }
 
-htmlContent += `
-</body>
-</html>
-`;
+// list all icons in the first layer of directories in the iconsPath folder
+const icons = iconsIterator(iconsPath);
 
-// Write HTML content to a file
-fs.writeFileSync(path.join(distPath, 'preview.html'), htmlContent);
+fs.writeFileSync(path.join(__dirname, 'list.json'), JSON.stringify(icons, null, 2));
